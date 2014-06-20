f933f0e6a8f44b491bef54150ef9d514dbdf976d
Michaela Lehr <michaela.lehr@geildanke.com>
2014-06-20 15:25:05 +0200

    Auto textarea completion

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,7 @@
  */
 angular.module('donotspyApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.name = '[Vorname Nachname]';
+    $scope.email = '[E-Mail-Adresse]';
+    $scope.domain = '[Betroffene Domain]';
   });
