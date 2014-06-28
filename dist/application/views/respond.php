526997d71c1f9731fcd1e88238997f0089b84a41
Richard Brammer <richard.brammer@geildanke.com>
2014-06-28 14:42:18 +0200

    Added "Share page" links on views/respond.php making it possible to share the page after mail has been verified.

--- dist/application/views/respond.php
+++ dist/application/views/respond.php
@@ -11,7 +11,11 @@ get_instance()->load->helper('url');
 </head>
 <body>
   <section class="l_centered">
-   <p><?php echo $respond; ?></p>
+    <p><?php echo $respond; ?></p>
+    <p>Seite teilen: </p>
+    <p>
+      <a href="https://alpha.app.net/intent/post?text=Ich%20habe%20gerade%20eine%20E-Mail%20an%20den%20Bundesnachrichtendienst%20geschrieben.%20%C3%9Cberwacht%20mich%20nicht!%20http://ueberwacht-mich-nicht.de/%20%23bnd" target="_blank">App.net</a> <a href="https://twitter.com/home?status=Ich%20habe%20gerade%20eine%20E-Mail%20an%20den%20Bundesnachrichtendienst%20geschrieben.%20%C3%9Cberwacht%20mich%20nicht!%20http://ueberwacht-mich-nicht.de/%20%23bnd" target="_blank">Twitter</a> <a href="https://plus.google.com/share?url=http://ueberwacht-mich-nicht.de/meine-emails/" target="_blank">Google Plus</a> <a href="https://www.facebook.com/sharer/sharer.php?u=http://ueberwacht-mich-nicht.de/" target="_blank">Facebook</a>
+    </p>
   </section>
 </body>
 </html>
